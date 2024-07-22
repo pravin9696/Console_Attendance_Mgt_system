6aa16de7c93907712f4bdd70155b4b6c6aeff0ab
pravin gaikwad <pravingaikwad.sibar@outlook.com>
2024-07-22 10:09:20 +0530

    teacher module with report completed

--- Teachers/TeacherTask.cs
+++ Teachers/TeacherTask.cs
@@ -1,18 +1,13 @@
 ﻿using Console_Attendance_Mgt_system.login_signup;
 using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Console_Attendance_Mgt_system.Teachers
 {
     internal class TeacherTask
     {
-        
+
 
         public void getTeacherID(out int? Tid)
         {
@@ -24,9 +19,9 @@ namespace Console_Attendance_Mgt_system.Teachers
                 SqlCommand cmd = new SqlCommand($"select teacher_id from tblTeachers where userid={Global.userid}");
                 cmd.Connection = con;
                 SqlDataReader rdr = cmd.ExecuteReader();
-                if (rdr.Read()) 
+                if (rdr.Read())
                 {
-                    Tid = rdr.GetInt32(0); 
+                    Tid = rdr.GetInt32(0);
                 }
                 else
                 {
@@ -44,23 +39,26 @@ namespace Console_Attendance_Mgt_system.Teachers
         }
 
         public void TeacherDashboard()
-        {            
+        {
 
             Console.WriteLine("--------Teacher DashBoard-------------");
             int Tchoice;
             do
-            {            
-            Console.WriteLine("1: Update Profile\n2: Show Profile\n3:Take Attendance");
-            Console.WriteLine("Enter Choice");
-            Tchoice = Convert.ToInt32(Console.ReadLine());
-            switch (Tchoice)
             {
-                case 1: updateTeacherProfile();break;
-                case 2: showTeacherProfile();break;
-                case 3: TakeAttendance();break;
-                default:
-                    break;
-            }
+                Console.WriteLine("1: Update Profile\n2: Show Profile\n3:Take Attendance\n 4: Show Report");
+                Console.WriteLine("Enter Choice");
+                Tchoice = Convert.ToInt32(Console.ReadLine());
+                switch (Tchoice)
+                {
+                    case 1: updateTeacherProfile(); break;
+                    case 2: showTeacherProfile(); break;
+                    case 3: TakeAttendance(); break;
+                    case 4: int? tid = -1;
+                        getTeacherID(out tid);
+                        Teacher_reports.Show_Attendance_Class_subjectWise(tid); break;
+                    default:
+                        break;
+                }
                 Console.WriteLine("Do you want to continue (Y/N)");
                 char ch2 = Console.ReadLine()[0];
                 if (ch2 == 'N' || ch2 == 'n')
@@ -70,13 +68,13 @@ namespace Console_Attendance_Mgt_system.Teachers
             } while (true);
         }
         public void showTeacherProfile()
-        {        
+        {
             if (Global.userid != null)
             {
                 SqlConnection con = new SqlConnection();
                 try
                 {
-                    
+
                     con.ConnectionString = Properties.Settings.Default.MyConString;
                     con.Open();
                     SqlCommand cmd = new SqlCommand();
@@ -86,14 +84,14 @@ namespace Console_Attendance_Mgt_system.Teachers
                     SqlDataReader rdr = cmd.ExecuteReader();
                     if (rdr.Read())
                     {
-                        
+
                         Console.WriteLine("First Name :" + rdr["Fname"]);
                         Console.WriteLine($"Last Name : {rdr["Lname"].ToString()}");
                     }
                     else
                     {
                         Console.WriteLine($"Profile of {Global.userid} is not available.....");
-                    }                   
+                    }
 
                 }
                 catch (Exception ex)
@@ -135,17 +133,17 @@ namespace Console_Attendance_Mgt_system.Teachers
             {
                 con.Close();
             }
-            
-           
+
+
 
         }
         public void updateTeacherProfile()
         {
-            if (Global.userid!=null)
+            if (Global.userid != null)
             {
-            Console.WriteLine("Enter First Name and Last Name");
-            string fname = Console.ReadLine();
-            string lname = Console.ReadLine();
+                Console.WriteLine("Enter First Name and Last Name");
+                string fname = Console.ReadLine();
+                string lname = Console.ReadLine();
                 SqlConnection con = new SqlConnection(Properties.Settings.Default.MyConString);
                 con.Open();
                 SqlCommand cmd = new SqlCommand();
@@ -153,7 +151,7 @@ namespace Console_Attendance_Mgt_system.Teachers
                 bool temp = checkUserIDinTeacherTable(Global.userid);
                 if (temp == true)
                 {
-                    cmd.CommandText = $"update tblTeachers set Fname='{fname}',Lname='{lname}' where userid="+Global.userid;
+                    cmd.CommandText = $"update tblTeachers set Fname='{fname}',Lname='{lname}' where userid=" + Global.userid;
                 }
                 else
                 {
@@ -161,7 +159,7 @@ namespace Console_Attendance_Mgt_system.Teachers
                 }
 
                 int n = cmd.ExecuteNonQuery();
-                if (n>0)
+                if (n > 0)
                 {
                     Console.WriteLine("Record inserted / updated Successfully....");
                 }
@@ -172,12 +170,12 @@ namespace Console_Attendance_Mgt_system.Teachers
             }
             else
             {
-                Console.WriteLine("Please login First");                
+                Console.WriteLine("Please login First");
                 new Login_signup().login();
             }
         }
 
-        public SqlDataReader GetStudents(int class_id)
+        public static SqlDataReader GetStudents(int class_id)
         {
 
             SqlConnection con = new SqlConnection(Properties.Settings.Default.MyConString);
@@ -187,14 +185,14 @@ namespace Console_Attendance_Mgt_system.Teachers
             SqlDataReader rdr = cmd.ExecuteReader();
             return rdr;
         }
-        public void getSubjectDetails(int classID,out int subId,out string subName)
+        public static void getSubjectDetails(int classID, out int subId, out string subName)
         {
-            
+
             subId = -1;
             subName = "";
             SqlConnection con = new SqlConnection(Properties.Settings.Default.MyConString);
             con.Open();
-            SqlCommand cmd = new SqlCommand("select * from tblSubjects where class_id="+classID);
+            SqlCommand cmd = new SqlCommand("select * from tblSubjects where class_id=" + classID);
             cmd.Connection = con;
             SqlDataReader rdr = cmd.ExecuteReader();
             Console.WriteLine("Subject ID\tSubject Name");
@@ -205,7 +203,7 @@ namespace Console_Attendance_Mgt_system.Teachers
             Console.WriteLine("Enter Subject ID");
             int sid = int.Parse(Console.ReadLine());
             //-----------------------------------------------------------------------
-            SqlCommand cmd1 = new SqlCommand($"select * from tblSubjects where class_id={classID} and subject_id={sid}",con);
+            SqlCommand cmd1 = new SqlCommand($"select * from tblSubjects where class_id={classID} and subject_id={sid}", con);
             rdr.Close();
             SqlDataReader temprdr = cmd1.ExecuteReader();
             if (temprdr.Read())
@@ -220,10 +218,10 @@ namespace Console_Attendance_Mgt_system.Teachers
             }
 
             con.Close();
-           
+
 
         }
-        public void getClassID(out int clsid,out string clsName)
+        public static void getClassID(out int clsid, out string clsName)
         {
             clsid = -1;
             clsName = "";
@@ -231,71 +229,72 @@ namespace Console_Attendance_Mgt_system.Teachers
             con.Open();
             SqlCommand cmd = new SqlCommand("select * from tblClasses");
             cmd.Connection = con;
-            SqlDataReader rdr=cmd.ExecuteReader();
-            
+            SqlDataReader rdr = cmd.ExecuteReader();
+
             Console.WriteLine("Class ID\tClass Name");
-            while (rdr.Read()) {
+            while (rdr.Read())
+            {
                 Console.WriteLine(rdr[0] + "\t" + rdr[1]);
-            }            
+            }
             Console.WriteLine("Enter Class ID");
             int cid = int.Parse(Console.ReadLine());
             //-----------------------------------------------------------------------
-            SqlCommand cmd1 = new SqlCommand("select *from tblClasses where class_id=" + cid,con);
+            SqlCommand cmd1 = new SqlCommand("select *from tblClasses where class_id=" + cid, con);
             rdr.Close();
             SqlDataReader temprdr = cmd1.ExecuteReader();
             if (temprdr.Read())
-            {               
-                    clsid = cid;
-                    clsName = temprdr.GetString(1);               
-            }            
-            
+            {
+                clsid = cid;
+                clsName = temprdr.GetString(1);
+            }
+
             con.Close();
-            
+
         }
-    
+
         public void TakeAttendance()
         {
             //----------- collect class id, class Name and respective student list 
             int classId;
             string className;
-            getClassID(out classId,out className);
+            getClassID(out classId, out className);
 
             SqlDataReader studsRdr = GetStudents(classId);
-            
+
             //----------------- collect system date
             DateTime dt = DateTime.Now;
-            
+
             //----------------- collect subject ID and subject Name
             string subName = "";
             int subId = 0;
             getSubjectDetails(classId, out subId, out subName);
             //--------show all students and add attendance in attendance table--------------
             Console.Clear();
-            Console.WriteLine("Date :"+dt.ToShortDateString());
-            Console.WriteLine("Class:"+className);
-            Console.WriteLine("Subject :"+subName);
+            Console.WriteLine("Date :" + dt.ToShortDateString());
+            Console.WriteLine("Class:" + className);
+            Console.WriteLine("Subject :" + subName);
             Console.WriteLine("-------Student list--------");
             int studId;
-            int ?TeacherID = null;
+            int? TeacherID = null;
             getTeacherID(out TeacherID);
             while (studsRdr.Read())
             {
-                studId= studsRdr.GetInt32(0);
-                Console.WriteLine( studsRdr[2] + "\t" + studsRdr[3]);
+                studId = studsRdr.GetInt32(0);
+                Console.WriteLine(studsRdr[2] + "\t" + studsRdr[3]);
                 Console.WriteLine("1 : Present\n0:Absent\n Please enter number:");
                 int attendanceMark = int.Parse(Console.ReadLine());
                 markAttendance(TeacherID, studId, subId, dt, attendanceMark);
             }
             Console.WriteLine("----Attendance Completed Successfully---------------..");
         }
-        public void markAttendance(int? tid,int stid,int subid,DateTime dt,int status)
+        public void markAttendance(int? tid, int stid, int subid, DateTime dt, int status)
         {
-          
+
             //SqlCommand cmd = new SqlCommand($"insert into tblAttendance(teacher_id, student_id, subject_id, Attendance_Date, status)values({tid},{stid}, {subid}, '{dt}', {status})");
 
             //SqlCommand cmd = new SqlCommand($"insert into tblAttendance values({tid},{stid}, {subid}, '{DateTime.Parse(dt.ToShortDateString())}', {status})");
             //cmd.Connection = con;
-           // int n=cmd.ExecuteNonQuery();
+            // int n=cmd.ExecuteNonQuery();
             //if (n > 0) {
             //    Console.WriteLine("Attendance marked successfully..");
             //}
@@ -313,15 +312,15 @@ namespace Console_Attendance_Mgt_system.Teachers
             dr["subject_id"] = subid;
             dr["Attendance_Date"] = dt;
             dr["status"] = status;
-            
+
 
             ds.Tables["tblAttendance"].Rows.Add(dr);
 
-           int n= adp.Update(ds, "tblAttendance");
+            int n = adp.Update(ds, "tblAttendance");
             if (n > 0)
-              {
-                   Console.WriteLine("Attendance marked successfully..");
-              }
+            {
+                Console.WriteLine("Attendance marked successfully..");
             }
         }
+    }
 }
---
+++ Teachers/Teacher_reports.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Attendance_Mgt_system.Teachers
+{
+    internal class Teacher_reports
+    {
+        public static void Show_Attendance_Class_subjectWise(int? teacher_id)
+        {
+            int classid=-1;
+            string className="";
+            TeacherTask.getClassID(out classid,out className);
+            DateTime dt;
+            Console.WriteLine("Enter Date to Show Attendance in DD-MM-YY format");
+            dt = DateTime.Parse(Console.ReadLine());
+
+            int subId;
+            string subName;
+            TeacherTask.getSubjectDetails(classid,out subId,out subName);
+            Console.WriteLine("1: Present students\n2:Absent Student:\n3:Present & Absent Students\nEnter choice");
+            int ch=int.Parse(Console.ReadLine());
+
+            //--- fetch records using join
+
+            SqlConnection con = new SqlConnection(Properties.Settings.Default.MyConString);
+            con.Open();
+            Console.WriteLine("########3date is:"+dt);
+            SqlCommand cmd = new SqlCommand($"select ta.student_id,status,Fname,Lname from tblAttendance ta inner join tblStudent ts on ta.student_id=ts.student_id where subject_id={subId} AND  Attendance_Date=@attDate AND teacher_id={teacher_id} order by status,student_id ", con);
+            cmd.Parameters.Add("@attDate", SqlDbType.Date).Value = dt;
+            cmd.CommandType = CommandType.Text;
+            SqlDataReader rdr= cmd.ExecuteReader();
+            Console.WriteLine("Student ID\tFirst Name\t Last Name");
+            while (rdr.Read())
+            {
+
+                if (ch == 1)  //for present student only
+                {
+                    if (rdr.GetBoolean(1))
+                    {
+                        Console.WriteLine($"{rdr[0]}\t\t{rdr[2]}\t\t{rdr[3]}");
+                    }
+                }
+                else if (ch==2)//only absent
+                {
+                    if (rdr.GetBoolean(1)==false)
+                    {
+                        Console.WriteLine($"{rdr[0]}\t\t{rdr[2]}\t\t{rdr[3]}");
+                    }
+                }
+                else  // all present and absent
+                {                    
+                    
+                        Console.WriteLine($"{rdr[0]}\t\t{rdr[2]}\t\t{rdr[3]}");
+                    
+                }
+            }
+            con.Close();
+
+
+        }
+    }
+}
